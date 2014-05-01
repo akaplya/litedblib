c9b96c5095433aeaee10d179a65cc2c82c5c5587
Anton Kaplya <akaplya@ebay.com>
2014-05-02 02:38:19 +0300

    demo-test changed

--- test2.php
+++ test2.php
@@ -15,9 +15,9 @@ $productFactory = new \Demo\ProductFactory();
 $productMapper = new Mapper(new Dml(), $connection, $productFactory);
 $t = 0;
 $time = microtime(true);
-for ($i = 1; $i <= 100; $i++) {
-    $raw = $productFactory->create(['sku' => 'sku#' . rand(1, 10)], false);
-    $raw->setName('name#' . rand(1, 10));
+for ($i = 1; $i <= 10000; $i++) {
+    $raw = $productFactory->create(['sku' => 'sku#' . rand(1, 10000)], false);
+    $raw->setName('name#' . rand(1, 10000));
 
     if ($raw->hasChanges()) {
         $t++;
