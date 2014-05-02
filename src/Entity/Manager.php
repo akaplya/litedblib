856226b3a4bc934f36a09a3daf06db3680d4eaab
Anton Kaplya <akaplya@ebay.com>
2014-05-02 23:01:37 +0300

    Nothing serious

--- src/Entity/Manager.php
+++ src/Entity/Manager.php
@@ -2,8 +2,24 @@
 
 namespace Entity;
 
+use Entity\UnitOfWork;
 
 class Manager
 {
+    /**
+     * @var UnitOfWork
+     */
+    protected $unitOfWork;
 
+    public function __construct(
+        UnitOfWork $unitOfWork
+    )
+    {
+        $this->unitOfWork = $unitOfWork;
+    }
+
+    public function getRepository($entity)
+    {
+
+    }
 } 
--- src/Entity/UnitOfWork.php
+++ src/Entity/UnitOfWork.php
@@ -3,6 +3,7 @@
 namespace Entity;
 
 use Entity\Config;
+use Entity\Repository;
 
 class UnitOfWork
 {
@@ -35,27 +36,34 @@ class UnitOfWork
         return $this->repositories[$code];
     }
 
+    public function flushRepository(Repository $repository)
+    {
+        $changed = 0;
+        foreach($repository->toArray() as $entity) {
+            /**@var \Entity\WrapperInterface $entity */
+            if ($entity->hasChanges()) {
+                $changed++;
+                if ($repository->getMapper()->exists($entity->getIdentifier())) {
+                    $repository->getMapper()->update($entity);
+                } else {
+                    $repository->getMapper()->create($entity);
+                }
+            }
+        }
+        echo get_class($repository) . " with (" . count($repository->toArray()) . "/" . $changed
+            . ") rows has been successfully processed! \n";
+        return $this;
+    }
+
     /**
      *
      */
     public function flush()
     {
         //todo: la-la-la-la implement sorting of possible repositories
-        foreach ($this->repositories as $code => $repository) {
-            /**@var \Entity\WrapperInterface $entity */
-            $changed = 0;
-            foreach($repository->toArray() as $entity) {
-                if ($entity->hasChanges()) {
-                    $changed++;
-                    if ($repository->getMapper()->exists($entity->getIdentifier())) {
-                        $repository->getMapper()->update($entity);
-                    } else {
-                        $repository->getMapper()->create($entity);
-                    }
-                }
-            }
-            echo $code . " with (" . count($repository->toArray()) . "/" . $changed
-                . ") rows has been successfully processed! \n";
+        foreach ($this->repositories as $repository) {
+            $this->flushRepository($repository);
         }
+        return $this;
     }
 }
